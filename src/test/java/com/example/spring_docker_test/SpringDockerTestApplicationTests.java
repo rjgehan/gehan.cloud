e629f226917b252706b40e587d5b5a6dd5ecc96d
rjgehan <70718521+rjgehan@users.noreply.github.com>
2026-05-13 21:12:36 -0400

    Initial commit

---
+++ src/test/java/com/example/spring_docker_test/SpringDockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_docker_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
